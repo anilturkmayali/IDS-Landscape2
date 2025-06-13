dada046911155adefaa53c3326717fa55717235e
Anil Turkmayali <77683020+anilturkmayali@users.noreply.github.com>
2025-06-13 12:13:30 +0200

    Create index.tsx
    Signed-off-by: Anil Turkmayali <77683020+anilturkmayali@users.noreply.github.com>

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>IDSA Landscape</h1>
+      <p>This is a placeholder. Landscape content will be added here soon.</p>
+    </main>
+  );
+};
+
+export default Home;
